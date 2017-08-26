f582c7317201f79030f9cfe35aeee910b8a3ceb5
WeaponZhi <584098488@qq.com>
2017-08-26 19:53:12 +0800

    插件实体类

--- app/src/main/java/org/sojex/stockquotes/classloaderplugin/plugin/PluginInfo.java
+++ app/src/main/java/org/sojex/stockquotes/classloaderplugin/plugin/PluginInfo.java
@@ -5,11 +5,14 @@ import android.content.res.AssetManager;
 import dalvik.system.DexClassLoader;
 
 /**
- * Created by admin on 2017/8/9.
+ * PluginInfo 插件实体类
+ * author:张冠之
+ * time: 2017/8/26 下午7:52
+ * e-mail: guanzhi.zhang@sojex.cn
  */
 
 public class PluginInfo {
-
+    //插件实体需要管理的资源，也可以管理很多其他的资源
     public DexClassLoader mClassLoader;
     public AssetManager mAsserManager;
     public Resources mResources;

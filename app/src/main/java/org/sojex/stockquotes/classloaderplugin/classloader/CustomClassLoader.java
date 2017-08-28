66b3ed4718ed5c47b2681947e52c37a9807595db
WeaponZhi <584098488@qq.com>
2017-08-28 10:23:27 +0800

    自定义 ClassLoader

--- app/src/main/java/org/sojex/stockquotes/classloaderplugin/classloader/CustomClassLoader.java
+++ app/src/main/java/org/sojex/stockquotes/classloaderplugin/classloader/CustomClassLoader.java
@@ -7,7 +7,12 @@ import java.io.InputStream;
 import dalvik.system.DexClassLoader;
 
 /**
- * Created by admin on 2017/8/9.
+ * CustomClassLoader 自定义 ClassLoader
+ * 插件化框架替代 Android 系统流程，要为每个插件 APK 创建 ClassLoader
+ * 防止插件中的类路径一致，导致插件中同名类无法被加载
+ * author:张冠之
+ * time: 2017/8/28 上午10:14
+ * e-mail: guanzhi.zhang@sojex.cn
  */
 
 public class CustomClassLoader extends DexClassLoader {
@@ -15,6 +20,12 @@ public class CustomClassLoader extends DexClassLoader {
         super(dexPath, optimizedDirectory, librarySearchPath, parent);
     }
 
+    /**
+     * 定义 ClassLoder 要以何种策略加载 class 文件
+     * @param name
+     * @return
+     * @throws ClassNotFoundException
+     */
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         byte[] classData = getClassData(name);
